af3e2ca26cd7fb6e1bb5c6605b0e0ed3fce6a592
Alexander <alexskvr03@gmail.com>
2022-03-12 18:38:37 +0300

    shared_ptr

--- Labs2/C++/Lab 2/unique_ptr/My_unique_ptr.h
+++ Labs2/C++/Lab 2/unique_ptr/My_unique_ptr.h
@@ -78,19 +78,26 @@ Type* My_unique_ptr<Type>::operator->()
 	return pointer;
 }
 
+template <typename Type, typename... Args>
+My_unique_ptr<Type> make_unique_ptr(Args&&... args)
+{
+	return My_unique_ptr<Type>(new Type(std::forward<Args>(args)...));
+}
 
 
-template <class Type>
+
+template <typename Type>
 class My_shared_ptr
 {
 public:
 	My_shared_ptr(Type* pointer = nullptr);
+	My_shared_ptr(const My_shared_ptr&);
+	My_shared_ptr(My_shared_ptr&&);
+	int get_count() const;
+	void operator=(My_shared_ptr&) const;
+	Type& operator*() const;
+	Type* operator->() const;
 	~My_shared_ptr();
-	My_shared_ptr(My_shared_ptr&);
-	Type* get();
-	void operator=(My_shared_ptr&);
-	Type& operator*();
-	Type* operator->();
 private:
 	Type* pointer;
 	int* count;
@@ -98,25 +105,11 @@ private:
 
 template <class Type>
 My_shared_ptr<Type>::My_shared_ptr(Type* pointer)
-	: pointer(pointer)
-{}
+	: pointer(pointer), count(new int(1)) {}
+
 
 template <class Type>
-My_shared_ptr<Type>::~My_shared_ptr()
-{
-	if (!pointer)
-	{
-		return;
-	}
-	if (!--(*count))
-	{
-		delete pointer;
-		delete count;
-	}
-}
-
-template <class Type>
-My_shared_ptr<Type>::My_shared_ptr(My_shared_ptr& new_pointer)
+My_shared_ptr<Type>::My_shared_ptr(const My_shared_ptr& new_pointer)
 	:pointer(new_pointer.pointer), count(new_pointer.count)
 {
 	if (!pointer)
@@ -126,5 +119,58 @@ My_shared_ptr<Type>::My_shared_ptr(My_shared_ptr& new_pointer)
 	++(*count);
 }
 
+template <class Type>
+My_shared_ptr<Type>::My_shared_ptr(My_shared_ptr&& new_pointer)
+	: pointer(new_pointer.pointer), count(new_pointer.count)
+{
+	new_pointer.pointer = nullptr;
+	new_pointer.count = nullptr;
+}
+
+template <class Type>
+int My_shared_ptr<Type>::get_count() const
+{
+	return *count;
+}
+
+template <class Type>
+void My_shared_ptr<Type>::operator=(My_shared_ptr& new_pointer) const
+{
+	delete pointer;
+	pointer = new_pointer.pointer;
+	new_pointer.pointer = nullptr;
+}
 
 
+template <class Type>
+Type& My_shared_ptr<Type>::operator*() const
+{
+	assert(pointer);
+	return *pointer;
+}
+
+template <class Type>
+Type* My_shared_ptr<Type>::operator->() const
+{
+	assert(pointer);
+	return pointer;
+}
+
+template <class Type>
+My_shared_ptr<Type>::~My_shared_ptr()
+{
+	if (*count > 1)
+	{
+		--(*count);
+		return;
+	}
+	delete pointer;
+	delete count;
+}
+
+template <typename Type, typename... Args>
+My_shared_ptr<Type> make_shared_ptr(Args&&... args)
+{
+	auto pointer = new Type(std::forward<Args>(args)...);
+	return My_shared_ptr<Type>(pointer);
+}//author - Vlad S.
