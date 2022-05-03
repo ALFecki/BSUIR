6a56432f28322aaa85ed390c5b06f760986dce6d
Alexander <alexskvr03@gmail.com>
2022-05-03 23:44:20 +0300

    lab 6

---
+++ C Sharp/Lab 6/Classes/Flower.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    abstract class Flower
+    {
+        private string name;
+        private string growthField;
+        //private string color;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string GrowthField
+        {
+            get => growthField;
+            set => growthField = value;
+        }        
+        /*public string Color_
+        {
+            get => color;
+            set => color = value;
+        }*/
+        public Flower(string name_)
+        {
+            Name = name_;
+        }
+
+        public abstract void PrintData();
+        
+        public virtual void Growth_Field(string field)
+        {
+            GrowthField = "Default";
+        }
+
+    }
+}
---
+++ C Sharp/Lab 6/Classes/Сhamomile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Сhamomile : Flower
+    {
+        private string field;
+
+
+        public Chamomile(string name)
+                : base(name)
+        {
+            Flower.GrowthField("");
+        }
+
+    }
+}
