6a6bbff8ef6062e9f320485e94b69e5c230066eb
ALFecki <alexskvr03@gmail.com>
2023-02-21 16:10:09 +0300

    lab 4 started

---
+++ C Sharp/4 sem/lab_1/lab_1/lab 4/IRateService.cs
@@ -0,0 +1,9 @@
+﻿using lab_1.lab_4.Entities;
+
+namespace lab_1.lab_4
+{
+    public interface IRateService
+    {
+        public IEnumerable<Rate> GetRates();
+    }
+}
---
+++ C Sharp/4 sem/lab_1/lab_1/lab 4/RateService.cs
@@ -0,0 +1,45 @@
+﻿
+using lab_1.lab_3.Entities;
+using lab_1.lab_4.Entities;
+using System.Text.Json;
+
+namespace lab_1.lab_4 {
+    class RateService : IRateService {
+
+        private HttpClient client;
+
+        public RateService() {
+            client = new();
+
+        }
+
+        async Task<IEnumerable<Currency>> GetCurrencies() {
+            var rates = new List<Currency>();
+            Uri uri = new Uri("https://www.nbrb.by/api/exrates/currencies");
+            try {
+
+                HttpResponseMessage response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode) {
+                    string content = await response.Content.ReadAsStringAsync();
+                    rates = JsonSerializer.Deserialize<List<Currency>>(content);
+                }
+                if (rates is not null) {
+                    foreach (Currency currency in rates) {
+                        if (currency.Cur_DateEnd < DateTime.Now) {
+                            rates.Remove(currency);
+                        }
+                    }
+                }
+                return rates;
+
+            } catch (Exception) {
+
+                return null;
+            }
+
+        }
+        public IEnumerable<Rate> GetRates() {
+            return null;
+        }
+    }
+}
