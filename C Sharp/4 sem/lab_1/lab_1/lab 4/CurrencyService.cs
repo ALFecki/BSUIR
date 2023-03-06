b16e08db1034ab1242ac5d1fd2f4dec1635c84ff
ALFecki <alexskvr03@gmail.com>
2023-03-06 09:06:09 +0300

    rep cleaning

---
+++ C Sharp/4 sem/lab_1/lab_1/lab 4/CurrencyService.cs
@@ -0,0 +1,39 @@
+﻿using lab_1.lab_3.Entities;
+using System.Text.Json;
+
+
+namespace lab_1.lab_4 {
+    public class CurrencyService {
+
+        HttpClient client;
+
+        public CurrencyService(HttpClient client) {
+            this.client = client;
+        }
+
+        public async Task<IEnumerable<Currency>> GetCurrencies() {
+            var currencies = new List<Currency>();
+            try {
+
+                HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+                if (response.IsSuccessStatusCode) {
+                    string content = await response.Content.ReadAsStringAsync();
+                    currencies = JsonSerializer.Deserialize<List<Currency>>(content);
+                }
+                if (currencies is not null) {
+                    foreach (Currency currency in currencies) {
+                        if (currency.Cur_DateEnd < DateTime.Now) {
+                            currencies.Remove(currency);
+                        }
+                    }
+                }
+                return currencies;
+
+            } catch (Exception) {
+
+                return null;
+            }
+        }
+
+    }
+}
